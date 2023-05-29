90a0c435fdf7da51863f5423c1c160ca6b7554f1
icchankun <ikkun.official@gmail.com>
2023-05-29 15:12:12 +0900

    JSONファイルのメモデータを取得し、そのデータを表示できるよう変更。

--- memos.rb
+++ memos.rb
@@ -1,7 +1,14 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'json'
 
 get '/' do
+  # JSONファイルからメモデータを読み込み
+  File.open("memos.json", "r") do |file|
+    hash = JSON.load(file)
+    @memos = hash["memos"]
+  end
+
   @title = "Top | メモアプリ"
   erb :index
 end
