f15ca66f9786e0aa560348f68f18e0c0ae4d119f
icchankun <ikkun.official@gmail.com>
2023-05-31 11:50:00 +0900

    メモ一覧画面からメモ詳細画面に遷移できるよう変更。

--- memos.rb
+++ memos.rb
@@ -12,3 +12,18 @@ get '/' do
   @title = "Top | メモアプリ"
   erb :index
 end
+
+get '/memos/:id' do
+  # JSONファイルからメモデータを読み込み
+  File.open("memos.json", "r") do |file|
+    hash = JSON.load(file)
+    @memos = hash["memos"]
+  end
+
+  # :idからindexを取得し、メモを特定する。
+  index = params[:id].to_i - 1
+  @memo = @memos[index]
+
+  @title = "show memo | メモアプリ"
+  erb :show
+end
