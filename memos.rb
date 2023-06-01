e52c7c0c5d748afcd8c814f6e8e0d07c4841c501
icchankun <ikkun.official@gmail.com>
2023-06-01 11:41:45 +0900

    メモを追加できるよう、メモ登録画面とメモ追加機能を作成。

--- memos.rb
+++ memos.rb
@@ -6,14 +6,37 @@ before do
   # JSONファイルからメモデータを読み込む。
   File.open("memos.json", "r") do |file|
     # JSONのメモデータをキーがシンボルのハッシュに変更。
-    hash = JSON.load(file, nil, symbolize_names: true, create_additions: false)
-    @memos = hash[:memos]
+    @hash = JSON.load(file, nil, symbolize_names: true, create_additions: false)
+    @memos = @hash[:memos]
   end
 end
 
 get '/' do
   @title = "Top | メモアプリ"
   erb :index
+end
+
+get '/memos/new' do
+  @title = "New memo | メモアプリ"
+  erb :new
+end
+
+post '/memos' do
+  # フォームに入力されたメモデータを取得し、ハッシュとして格納.
+  title = params[:title]
+  body = params[:body]
+  memo = {title: "#{title}", body: "#{body}"}
+  @memos << memo
+  @hash[:memos] = @memos
+
+  # JSONファイルへメモデータを書き込む。
+  File.open("memos.json", "w") do |file|
+    JSON.dump(@hash, file)
+  end
+
+  # メモデータを保存後、そのメモの詳細画面に遷移。
+  id = @memos.size
+  redirect "/memos/#{id}"
 end
 
 get '/memos/:id' do
