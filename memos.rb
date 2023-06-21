0bfb7859b5b3f14c150a46f31545409c14bd3e8d
icchankun <ikkun.official@gmail.com>
2023-06-21 11:46:44 +0900

    コードを読みやすくするため、ルーティングハンドラー内に空行を追加。

--- memos.rb
+++ memos.rb
@@ -34,54 +34,70 @@ end
 
 get '/memos' do
   @memos = fetch_memos
+
   @page_title = 'Top'
+
   erb :index
 end
 
 get '/memos/new' do
   @page_title = 'New memo'
+
   erb :new
 end
 
 post '/memos' do
   memos = fetch_memos
+
   title = params[:title]
   body = params[:body]
   memo = { id: SecureRandom.uuid, title:, body: }
+
   memos << memo
   write_to_file(memos)
+
   redirect "/memos/#{memo[:id]}"
 end
 
 get '/memos/:id' do
   @memo = find_memo(params[:id])
   pass if !@memo
+
   @page_title = 'show memo'
+
   erb :show
 end
 
 get '/memos/:id/edit' do
   @memo = find_memo(params[:id])
   pass if !@memo
+
   @page_title = 'Edit memo'
+
   erb :edit
 end
 
 patch '/memos/:id' do
   memos = fetch_memos
+
   memo = find_memo(params[:id])
+
   index = memos.index(memo)
   memos[index][:title] = params[:title]
   memos[index][:body] = params[:body]
   write_to_file(memos)
+
   redirect "/memos/#{params[:id]}"
 end
 
 delete '/memos/:id' do
   memos = fetch_memos
+
   memo = find_memo(params[:id])
+
   index = memos.index(memo)
   memos.delete_at(index)
   write_to_file(memos)
+
   redirect '/memos'
 end
